e26c5b24476bfb0420f5ad85cf65c2d90f1efb98
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2026-08-26 22:28:36 +0200

    Skip type checks in the `fmt` task

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
 	fmt: defineOxfmtConfig({ ignorePatterns: ["dist/**/*", "**/*.md"] }) as UserOxfmtConfig,
 	lint: defineOxlintConfig({
 		ignorePatterns: ["dist/**/*"],
+		options: { typeCheck: false },
 		overrides: [
 			{
 				files: [
@@ -43,7 +44,7 @@ export default defineConfig({
 		// language=sh
 		tasks: {
 			build: { command: "vp pack" },
-			check: { command: "vp check" },
+			check: { command: "vp lint --type-check" },
 			fmt: { command: "vp check --fix" },
 			install: { command: "vp install --frozen-lockfile --ignore-scripts", cache: false },
 			setup: { command: "node tools/setup.script.ts", cache: false },
