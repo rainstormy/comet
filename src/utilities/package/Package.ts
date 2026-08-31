19891482f078228ddaaf8566e0e9d1f94d375ef6
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2026-08-31 20:28:19 +0200

    Add import attribute to `package.json` imports

--- src/utilities/package/Package.ts
+++ src/utilities/package/Package.ts
@@ -1,5 +1,5 @@
 // oxlint-disable-next-line no-restricted-imports -- Access the `version` field directly from `package.json`.
-import * as packagejson from "../../../package.json"
+import * as packagejson from "../../../package.json" with { type: "json" }
 
 export function getPackageVersion(): string {
 	return packagejson.version
